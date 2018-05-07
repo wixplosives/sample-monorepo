b9e6226297a439b9c12c33ed6ad8f2ed20ac27ed
Avi Vahl <avi.vahl@wix.com>
2018-05-07 20:07:04 +0300

    Add first sample package-a - has separate tsconfigs for `src` and `test` that extend the base config, adding scenario-specific config (tests need mocha, etc) - builds to cjs/esm (matches several popular packages out there and retains folder depth)

---
+++ package-a/test/sub/folder/test2.spec.ts
@@ -0,0 +1,8 @@
+import { myValue } from '../../../src'
+import { expect } from 'chai';
+
+describe('sub folder test', () => {
+    it('simply works', () => {
+        expect(myValue).to.be.true
+    })
+})
---
+++ package-a/test/test.spec.ts
@@ -0,0 +1,8 @@
+import { myValue } from '../src'
+import { expect } from 'chai';
+
+describe('test', () => {
+    it('works', () => {
+        expect(myValue).to.be.true
+    })
+})
