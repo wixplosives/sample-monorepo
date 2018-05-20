74235c6b40544dd1084d25700df807056c6321ff
Avi Vahl <avi.vahl@wix.com>
2018-05-20 13:38:16 +0300

    Add HTML meta tags to generated SSR page

--- server/src/index.tsx
+++ server/src/index.tsx
@@ -19,16 +19,20 @@ app.use(express.static(appDistDirectory))
 app.get('/server', (_req, res) => {
   res.send(`
 <!DOCTYPE html>
-<html>
-  <head>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
-    <title>Webpack App</title>
-  </head>
-  <body>
-  <div id="SITE_MAIN" data-ssr>
-    ${ReactDOMServer.renderToString(<App text="Hello World (SSR!)" />)}
-  </div>
-  <script type="text/javascript" src="main.js"></script></body>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="Description" content="Monorepo example server-side renderer app">
+    <title>Monorepo Example</title>
+</head>
+<body>
+    <div id="SITE_MAIN" data-ssr>
+      ${ReactDOMServer.renderToString(<App text="Hello World (SSR!)" />)}
+    </div>
+    <script type="text/javascript" src="main.js"></script>
+</body>
 </html>
 `.trim())
   res.end()
