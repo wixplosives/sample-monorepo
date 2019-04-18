3e47579cfe2cbf5f293b5be93951a8cc9ea587f3
Jeremy <jeremy.k.coleman@outlook.com>
2019-04-18 15:50:51 -0400

    Update webpack.config.js

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -1,8 +1,14 @@
 const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack')
 
 module.exports = {
+    devServer: {
+        stats: "minimal",
+        hot: true
+    },
+    stats: "minimal",
     devtool: 'source-map',
     // root of the monorepo, so that paths in output will be clickable
     context: join(__dirname, '..', '..'),
@@ -23,9 +29,15 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.mjs', '.js', '.json']
+        extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json'],
+        alias:{
+            "react-dom":"@hot-loader/react-dom"
+        }
     },
     plugins: [
+        //new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)}),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
         new HtmlWebpackPlugin({ title: 'Sample Monorepo App' }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
@@ -33,3 +45,7 @@ module.exports = {
         })
     ]
 };
+
+//instead of dev/prod configs, i usually do a base, dev, and prod all in 1 file, and do something like config = env === 'prod' ? prod : dev
+//use webpack merge create the 2+ configs from the base, then just 
+//module.exports = config
