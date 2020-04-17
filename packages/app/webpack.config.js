b280979ad06031802e016736d95c2eddcd09d643
Avi Vahl <avi.vahl@wix.com>
2020-04-17 04:14:16 +0300

    webpack: actually build umd instead of defaulting to IIFE

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'umd'),
+    libraryTarget: 'umd',
   },
   plugins: [...rootWebpackConfig.plugins, new HtmlWebpackPlugin({ title: 'Sample Monorepo App' })],
 };
