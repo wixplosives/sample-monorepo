8c34eb167764ceedbcb50a09df8b352f87222788
Avi Vahl <avi.vahl@wix.com>
2020-11-23 00:41:04 +0200

    infra: ignore unused diagnostics during dev time

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
         options: {
           projectReferences: true,
           configFile: require.resolve('./tsconfig.json'),
+          compilerOptions: {
+            // build still catches these. avoid them during bunding time for a nicer dev experience.
+            noUnusedLocals: false,
+            noUnusedParameters: false,
+          },
         },
       },
       {
