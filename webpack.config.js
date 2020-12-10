ee588d22ceb9a46069248f508e06bcabb9408a82
Avi Vahl <avi.vahl@wix.com>
2020-12-10 20:06:51 +0200

    infra(webpack): avoid trying .mjs as an implicit extension in existing packages using .mjs files, these sources usually already target each other with an explicit extension, as Node resolution requires it.

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.mjs', '.js', '.json'],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
   },
   plugins: [
     new MiniCssExtractPlugin({
