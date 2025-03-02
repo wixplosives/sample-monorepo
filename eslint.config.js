de2f90b6fd7bf3ed90f6e399c9b20147b5446f2b
Avi Vahl <avi@vahl.co.il>
2025-03-02 06:04:12 +0200

    chore(deps-dev): typescript@5.8.2 - turn off typescript-eslint warnings about unsupported typescript versions - regenerate lock file

--- eslint.config.js
+++ eslint.config.js
@@ -29,7 +29,7 @@ export default [
     },
   },
   ...pluginTypescript.configs.recommendedTypeChecked,
-  { languageOptions: { parserOptions: { projectService: true } } },
+  { languageOptions: { parserOptions: { projectService: true, warnOnUnsupportedTypeScriptVersion: false } } },
   {
     files: ["**/*.{ts,tsx,mts,cts}"],
     rules: {
